4fa51a7077ffe66be9ed16568a67717ec37312ae
Luis Chrestia <chrestia.luis@gmail.com>
2023-08-08 16:42:25 -0300

    fix img specialidades

--- src/components/sectionDoctorsMain/index.jsx
+++ src/components/sectionDoctorsMain/index.jsx
@@ -8,22 +8,22 @@ export const SectionDoctorsMain = () => {
         <h3 className="especialidades-title">Especialidades</h3>
         <div className='cards-doctors-show'>
             <Routes>
-                <Route path="/" element={<SectionDoctors especialidad={"Cardiología"} image={'./src/images/especialidades/cardiologia.jpg'} />} />
+                <Route path="/" element={<SectionDoctors especialidad={"Cardiología"} image={'https://i.ibb.co/mRLj6Td/cardiologia.jpg'} />} />
             </Routes>
             <Routes>
-                <Route path="/" element={<SectionDoctors especialidad={"Dermatología"} image={'./src/images/especialidades/dermatologia.jpg'} />} />
+                <Route path="/" element={<SectionDoctors especialidad={"Dermatología"} image={'https://i.ibb.co/k1XVfPZ/dermatologia.jpg'} />} />
             </Routes>
             <Routes>
-                <Route path="/" element={<SectionDoctors especialidad={"Neurología"} image={'./src/images/especialidades/neurologia.jpg'} />} />
+                <Route path="/" element={<SectionDoctors especialidad={"Neurología"} image={'https://i.ibb.co/cF0CmyM/neurologia.jpg'} />} />
             </Routes>
             <Routes>
-                <Route path="/" element={<SectionDoctors especialidad={"Obstetricia"} image={'./src/images/especialidades/obstetricia.jpg'} />} />
+                <Route path="/" element={<SectionDoctors especialidad={"Obstetricia"} image={'https://i.ibb.co/DKbkfPh/obstretra.jpg'} />} />
             </Routes>
             <Routes>
-                <Route path="/" element={<SectionDoctors especialidad={"Odontología"} image={'./src/images/especialidades/odontologia.jpg'} />} />
+                <Route path="/" element={<SectionDoctors especialidad={"Odontología"} image={'https://i.ibb.co/CWmTtXm/odontologia.jpg'} />} />
             </Routes>
             <Routes>
-                <Route path="/" element={<SectionDoctors especialidad={"Traumatología"} image={'./src/images/especialidades/traumatologia.jpg'} />} />
+                <Route path="/" element={<SectionDoctors especialidad={"Traumatología"} image={'https://i.ibb.co/MRhqHX4/traumatologia.jpg'} />} />
             </Routes>
 </div>
       </div>)}
