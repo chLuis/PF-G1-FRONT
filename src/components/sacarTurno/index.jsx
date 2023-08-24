231357744b8c34001f39160956c77e8534fd79dd
Luis Chrestia <chrestia.luis@gmail.com>
2023-08-24 02:04:48 -0300

    fix panelUser, widget turnos, apiClima error. Ahora se pueden ver los motivos de los turnos

--- src/components/sacarTurno/index.jsx
+++ src/components/sacarTurno/index.jsx
@@ -1,18 +1,20 @@
 import './sacarTurno.css'
 import { useState  } from 'react'
-import { useNavigate } from 'react-router-dom'
+//import { useNavigate } from 'react-router-dom'
 
 export const SacarTurno = () => {
 
-    const Navigate = useNavigate()
+    //const Navigate = useNavigate()
     const [turnos, setTurnos] = useState(["turnos-float"])
+    const [turnos_especialidad, setTurnos_especialidad] = useState("turnos-float hide")
     const sacarTurno = () => {
         if(turnos  !== "turnos-float"){
             setTurnos("turnos-float hide")
-            setTimeout(() => {
-                Navigate("/turnos")
+            setTurnos_especialidad("turnos-float")
+            // setTimeout(() => {
+            //     Navigate("/turnos")
             
-            },1000)
+            // },1000)
         }
     }
     
@@ -20,6 +22,12 @@ export const SacarTurno = () => {
     return (
     <div className="sacarTurno">         
         <span className={turnos} onClick={sacarTurno}>Turno</span>
+        <span className={turnos_especialidad}>
+            <div className='div-turnos-component'>
+                <div className='btn-sacarTurno'><a href='/Turnos'>Sacar turno</a></div>
+                <div className='btn-misTurnos'><a href='/PanelUser'>Mis turnos</a></div>
+            </div>
+        </span>
     </div>
   )
 
